391c7c679ed69ac66cfe16ee2f2e3753e5a07a06
Abi Bromley <abigailstephanie@hotmail.co.uk>
2020-02-20 15:33:26 +0000

    Add table

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    {
+      name: 'Abi',
+      age: 27,
+      job: 'Unemployed Scrounger',
+      employed: false
+    },
+    {
+      name: 'Jos',
+      age: 29,
+      job: 'Annoying Know-It-All',
+      employed: true
+    },
+    {
+      name: 'Jack',
+      age: 15,
+      job: 'Bestest Good Boy',
+      employed: false
+    }
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'employed', label: 'Has a Job'},
+    { key: 'job', label: 'Job' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
