2aa7a01b3f1f4043602f034d66b6d55bede7b34d
Abi Bromley <abigailstephanie@hotmail.co.uk>
2020-02-21 11:24:20 +0000

    Add Accordion component

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class ModsHomeComponent implements OnInit {
   showModal: boolean = false;
 
+  accordionItems = [
+    { title: 'Why is the sky blue?', content: 'Because science.' },
+    { title: 'What does an orange taste like?', content: 'An orange tastes not unlike a satsuma or clementine, but does not taste like tuna.' },
+    { title: 'What colour is that cat?', content: 'The cat is orange (but does not taste like an orange!)'}
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
